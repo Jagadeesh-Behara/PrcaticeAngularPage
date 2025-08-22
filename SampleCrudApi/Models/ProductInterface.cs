61a81cd18cc2325ddf4fe34820197e7035a570d0
Behara Jagadeesh <beharajagadeesh123@gmail.com>
2025-08-22 12:57:11 +0530

    Create Product Interface

---
+++ SampleCrudApi/Models/ProductInterface.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleCrudApi.Models
+{
+    public interface ProductInterface
+    {
+        Task<Product> GetAllProducts();
+        Task<Product> GetProduct(int id);
+        Task<Product> CreateProduct(Product product);
+        Task<Product> UpdateProduct(int id, Product product);
+        Task<bool> DeleteProduct(int id);
+    }
+}

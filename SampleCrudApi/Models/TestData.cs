16a0c319dd1ba7244eb1fba6a34e7e2c8d8b30a6
Behara Jagadeesh <beharajagadeesh123@gmail.com>
2025-08-22 13:04:11 +0530

    Create TestData model

---
+++ SampleCrudApi/Models/TestData.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleCrudApi.Models
+{
+    public class TestData
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
